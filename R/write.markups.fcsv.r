68cd1fbc8ec56f12f1cb4209ebf4e5e8dd1b1f83
muratmaga <muratmaga@outlook.com>
2021-07-21 09:59:26 -0700

    Added to support RAS/LPS specification

--- R/write.markups.fcsv.r
+++ R/write.markups.fcsv.r
@@ -1,6 +1,7 @@
 #' Export landmark data into FCSV file
 #'
 #' This function export landmark coordinates into an FCSV file
+#' By default coordinates are assumed to be written in LPS, but can be change to RAS using the @param coord option.
 #' a fcsv file,
 #' @param pts a matrix/array that contains landmark coordinates \cr
 #' nrow = number of coordinates \cr
@@ -12,41 +13,49 @@
 #' The label for each landmark is the rowname of the input landmark matrix
 #' @examples
 #' lms = read.markups.fcsv(file = url("https://raw.githubusercontent.com/SlicerMorph/SampleData/master/Gorilla_template_LM1.fcsv"))
-#' write.markups.fcsv(pts=lms, outfile = "Your_Directory/test.fcsv")
+#' write.markups.fcsv(pts=lms, outfile = "Your_Directory/test.fcsv", coord="LPS")
 #' @export
 
-write.markups.fcsv = function(pts=NULL, outfile=NULL){
+write.markups.fcsv = function (pts = NULL, outfile = NULL, coord="LPS") {
 
-  temp = "# Markups fiducial file version = 4.13\n# CoordinateSystem = LPS\n# columns = id,x,y,z,ow,ox,oy,oz,vis,sel,lock,label,desc,associatedNodeID\n"
-
-  if (length(rownames(pts))==0) {
-    for (i in 1:nrow(pts))
-      temp = paste0(temp, '\n',
-                    paste(paste0("vtkMRMLMarkupsFiducialNode_", i-1),
-                          pts[i,1],
-                          pts[i,2],
-                          pts[i,3],
-                          paste(rep(0,3), collapse=','),
-                          paste(rep(1,4), collapse=','),
-                          paste0("F-", i),
-                          paste(rep("",2), collapse=','),
-                          sep=','))
-
-    cat (temp, file = outfile)
-  } else {
-    labels=rownames(pts)
-    for (i in 1:nrow(pts))
-      temp = paste0(temp, '\n',
-                    paste(paste0("vtkMRMLMarkupsFiducialNode_", i-1),
-                          pts[i,1],
-                          pts[i,2],
-                          pts[i,3],
-                          paste(rep(0,3), collapse=','),
-                          paste(rep(1,4), collapse=','),
-                          paste(labels[i]),
-                          paste(rep("",2), collapse=','),
-                          sep=','))
-
-    cat (temp, file = outfile)
-  }
+  if (toupper(coord) == "LPS") {  
+      temp = "# Markups fiducial file version = 4.13\n# CoordinateSystem = LPS\n# columns = id,x,y,z,ow,ox,oy,oz,vis,sel,lock,label,desc,associatedNodeID\n"
+      if (length(rownames(pts)) == 0) {
+        for (i in 1:nrow(pts)) temp = paste0(temp, "\n", paste(paste0("vtkMRMLMarkupsFiducialNode_", 
+                                                                      i - 1), pts[i, 1], pts[i, 2], pts[i, 3], paste(rep(0, 
+                                                                                                                         3), collapse = ","), paste(rep(1, 4), collapse = ","), 
+                                                               paste0("F-", i), paste(rep("", 2), collapse = ","), 
+                                                               sep = ","))
+        cat(temp, file = outfile)
+      }
+      else {
+        labels = rownames(pts)
+        for (i in 1:nrow(pts)) temp = paste0(temp, "\n", paste(paste0("vtkMRMLMarkupsFiducialNode_", 
+                                                                      i - 1), pts[i, 1], pts[i, 2], pts[i, 3], paste(rep(0, 
+                                                                                                                         3), collapse = ","), paste(rep(1, 4), collapse = ","), 
+                                                               paste(labels[i]), paste(rep("", 2), collapse = ","), 
+                                                               sep = ","))
+        cat(temp, file = outfile)
+      }
+  } else if (toupper(coord) == "RAS") {  
+    temp = "# Markups fiducial file version = 4.13\n# CoordinateSystem = RAS\n# columns = id,x,y,z,ow,ox,oy,oz,vis,sel,lock,label,desc,associatedNodeID\n"
+    if (length(rownames(pts)) == 0) {
+      for (i in 1:nrow(pts)) temp = paste0(temp, "\n", paste(paste0("vtkMRMLMarkupsFiducialNode_", 
+                                                                    i - 1), pts[i, 1], pts[i, 2], pts[i, 3], paste(rep(0, 
+                                                                                                                       3), collapse = ","), paste(rep(1, 4), collapse = ","), 
+                                                             paste0("F-", i), paste(rep("", 2), collapse = ","), 
+                                                             sep = ","))
+      cat(temp, file = outfile)
+    }
+    else {
+      labels = rownames(pts)
+      for (i in 1:nrow(pts)) temp = paste0(temp, "\n", paste(paste0("vtkMRMLMarkupsFiducialNode_", 
+                                                                    i - 1), pts[i, 1], pts[i, 2], pts[i, 3], paste(rep(0, 
+                                                                                                                       3), collapse = ","), paste(rep(1, 4), collapse = ","), 
+                                                             paste(labels[i]), paste(rep("", 2), collapse = ","), 
+                                                             sep = ","))
+      cat(temp, file = outfile)
+    }
+  } else print("unsupported Coordinate System")
+  
 }
