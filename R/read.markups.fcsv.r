73a2cf7fa15cb5617d269dafb84683495356961d
Zhang <Chi.Zhang@seattlechildrens.org>
2021-08-13 12:27:54 -0700

    Update read.markups.fcsv help doc

--- R/read.markups.fcsv.r
+++ R/read.markups.fcsv.r
@@ -11,7 +11,8 @@
 #' Row names are assigned from the labels in the FCSV file  \cr
 #' Column names are assigned as "x", "y", and "z", depending on the dimension  \cr
 #' @examples
-#' file = url("https://raw.githubusercontent.com/SlicerMorph/SampleData/master/Gorilla_template_LM1.fcsv")
+#' #forceLPS = FALSE
+#' file = "https://raw.githubusercontent.com/SlicerMorph/SampleData/master/Gorilla_template_LM1.fcsv"
 #' lms = read.markups.fcsv(file = file, forceLPS = FALSE) #default setting for forceLPS
 #' #Return a 41x3 matrix that stores raw landmark coordinates from the fcsv files; rownames = labels in the FCSV file; colnames = "x", "y", "z"
 #' lms[1:3, ]
@@ -20,6 +21,8 @@
 #' Gorilla_template_LM1-2 114.785 381.650 -128.2390
 #' Gorilla_template_LM1-3 109.137 294.534  -97.4347
 #'
+#' #forceLPS = TRUE
+#' file = "https://raw.githubusercontent.com/SlicerMorph/SampleData/master/Gorilla_template_LM1.fcsv"
 #' lms = read.markups.fcsv(file = file, forceLPS = TRUE)
 #' #Because forceLPS = TRUE, the function will read the "coordinateSystem" in the 2nd line of the fcsv to see if it is "LPS"
 #' x <- readLines(file, n = 2)
@@ -35,20 +38,25 @@
 #' @export
 
 read.markups.fcsv = function (file=NULL, forceLPS = FALSE) {
-  temp = read.csv(file=file, skip = 2, header = T)
-  LM = array (data=as.matrix( temp[,2:4]),
+  temp <- read.csv(file, skip = 2, header = T)
+  LM <- array (data=as.matrix( temp[,2:4]),
               dim =c(nrow(temp), 3),
               dimnames=list(temp$label, c("X", "Y", "Z")))
+  print(file)
 
   M_flip <- rbind(c(-1, 0, 0), c(0, -1, 0), c(0, 0, 1))
 
   if (forceLPS == TRUE){
-    x <- readLines(file, n = 2)
-    y <- strsplit(x, split = " ", fixed = T)[[2]][4] #return coordinate system type, either "0" or "LPS"
     #if the coordinate system = "0", flip the sign of the x and y coordinates to make it consistent with LPS coordinate system
     #if the coordinate system = "LPS", do nothing
-    if (y != "LPS")
-    LM <- LM%*%M_flip
+    x <- readLines(file, n = 2)
+    y <- strsplit(x, split = " ", fixed = T)[[2]][4] #return coordinate system type, either "0" or "LPS"
+    if (y != "LPS"){
+      LM <- LM%*%M_flip
+    }
+    else {
+      LM <- LM
+    }
   }
 
   return(LM)
