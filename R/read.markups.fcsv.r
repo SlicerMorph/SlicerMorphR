3f4fe4551ef689a7a9ee846a0e8cf459c3c421fc
Zhang <Chi.Zhang@seattlechildrens.org>
2021-08-13 14:36:19 -0700

    Update LPS logic

--- R/read.markups.fcsv.r
+++ R/read.markups.fcsv.r
@@ -57,11 +57,11 @@ read.markups.fcsv = function (file=NULL, forceLPS = FALSE) {
     #if the coordinate system = "LPS", do nothing
     x <- readLines(file, n = 2)
     y <- strsplit(x, split = " ", fixed = T)[[2]][4] #return coordinate system type, either "0" or "LPS"
-    if ( (y != "LPS") | (y != "1")) {
-      LM <- LM%*%M_flip
+    if ( (y == "LPS") | (y == "1")) {
+      LM <- LM
     }
     else {
-      LM <- LM
+      LM <- LM%*%M_flip
     }
   }
 
